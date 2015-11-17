c67da46bb1df8461b225bfff584ba6df29727f9a
efokoue <epfeqa@rit.edu>
2015-11-17 16:35:49 -0500

    Computation of the Computational Complexity

--- test-1.R
+++ test-1.R
@@ -8,6 +8,19 @@
    p <- 20
    x <- matrix(rnorm(n*p), nrow=n, ncol=p, byrow=TRUE)
 
+   P <- 50
+   d <- numeric(P-1)
+   l <- 0
 
+   for(p in 2:P)
+   {
+      l <- l + 1   
+      x <- matrix(rnorm(n*p), nrow=n, ncol=p, byrow=TRUE)
+      tm <- proc.time() 
+      det(cov(x)) 
+      d[l] <- (proc.time()-tm)[3]
+      
+   }   
 
-    
+   plot(2:P, d, type='l', main='Computational Complexity of determinant')
+
